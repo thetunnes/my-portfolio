e5055bf7685467424df1a70ac212b647ec59ae50
Tunnes <cauepani06@gmail.com>
2023-06-05 19:38:12 -0300

    fix: Problem in action, prisma error

--- src/app/jadesayuri/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ExtraPage() {
-  return (
-    <div>
-      <h2>A página do amor da minha vida &#x1F498;</h2>
-    </div>
-  )
-}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -28,11 +28,11 @@ export function Button({
     }
     ${
       (color === 'red' &&
-        'border border-transparent bg-red-500 text-gray-200 transition-colors duration-500 hover:bg-red-600 disabled:bg-transparent disabled:text-red-500') ||
+        'border border-transparent bg-red-500 text-gray-200 transition-colors duration-500 hover:bg-red-600 disabled:border-red-500 disabled:bg-transparent disabled:text-zinc-500') ||
       (color === 'green' &&
-        'border border-transparent bg-green-500 text-gray-200 transition-colors duration-500 hover:bg-green-600 disabled:bg-transparent disabled:text-green-500') ||
+        'border border-transparent bg-green-500 text-gray-200 transition-colors duration-500 hover:bg-green-600 disabled:border-green-500 disabled:bg-transparent disabled:text-zinc-500') ||
       (color === 'yellow' &&
-        'border border-transparent bg-yellow-500 text-gray-200 transition-colors duration-500 hover:bg-yellow-600 disabled:bg-transparent disabled:text-yellow-500') ||
+        'border border-transparent bg-yellow-500 text-gray-200 transition-colors duration-500 hover:bg-yellow-600 disabled:border-yellow-500 disabled:bg-transparent disabled:text-zinc-500') ||
       (color === 'default' &&
         'border border-solid border-zinc-800 bg-zinc-800 px-4 py-2 text-sm font-semibold text-gray-200 outline-none transition-all duration-150 ease-linear hover:bg-zinc-200 hover:text-zinc-800 focus:outline-none active:bg-zinc-500 dark:border-zinc-200 dark:bg-zinc-200 dark:text-zinc-800 dark:hover:bg-zinc-900 dark:hover:text-zinc-200')
     }
